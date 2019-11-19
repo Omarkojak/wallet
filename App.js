eda0fedc0a5927e593555a24a9f37befb4a201ff
Omarkojak <omarkojaks@gmail.com>
2019-11-19 23:53:22 +0100

    feat: Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+
+const navigator = createStackNavigator({
+  Home: HomeScreen
+}, {
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    title: 'Expenses'
+  }
+});
+
+export default createAppContainer(navigator);
